336d7ab37bed6fc65bea6f616ca4ab80453eefe2
Samuel COLLON <samuel.collon@wttj.co>
2021-05-20 11:03:16 +0200

    chore: upgrade connection to 1.1.0

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Sonix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:connection, "~> 1.0.4"},
+      {:connection, "~> 1.1.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
